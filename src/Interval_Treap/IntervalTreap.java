78216ab3755cfae9ba8d472a26e6398ff131fcd4
mambourn <mambourn@iastate.edu>
2020-04-02 20:33:10 -0500

    gitignore and rotate of insert

--- src/Interval_Treap/IntervalTreap.java
+++ src/Interval_Treap/IntervalTreap.java
@@ -50,7 +50,72 @@ public class IntervalTreap {
         }
 
         /* phase 2: rotate based on priority */
+        while(z.getParent()!= null){    //TODO: fix condition
+            boolean rotate = (z.getPriority() < z.getParent().getPriority());
+            int leftOrRight = 2;   //if unchanged, don't need to update imax, right = 0 and left = 1
+            if(rotate && (z.getParent().getLeft()== z)){
+                rightRotate(z);
+                leftOrRight = 0;   //right child of z needs imax update
+            }else if(rotate && (z.getParent().getRight() == z)){
+                leftRotate(z);
+                leftOrRight = 1;   //left child of z needs imax updat
+            }
 
+            if((leftOrRight != 2)) {        //update Imax
+                if(leftOrRight == 0){  //right
+                    Node right = z.getRight();
+                    //update right child's imax
+                    if(right.getRight() != null && right.getLeft() != null){    //both children exist
+                        int childImax = Math.max(right.getRight().getIMax(), right.getLeft().getIMax());
+                        right.setIMax(Math.max(right.getIMax(),childImax));
+                    }
+                    else if(right.getRight() != null && right.getLeft() == null) { //only right child
+                        right.setIMax(Math.max(right.getRight().getIMax(), right.getIMax()));
+                    }
+                    else if( right.getRight() == null && right.getLeft() != null){//only left child
+                        right.setIMax(Math.max(right.getLeft().getIMax(), right.getIMax()));
+                    }else{}
+
+                    // update z's imax
+                    if(z.getRight() != null && z.getLeft() != null){    //both children exist
+                        int zChildImax = Math.max(z.getRight().getIMax(), z.getLeft().getIMax());
+                        z.setIMax(Math.max(z.getIMax(),zChildImax));
+                    }
+                    else if(z.getRight() != null && z.getLeft() == null) { //only right child
+                        z.setIMax(Math.max(z.getRight().getIMax(), z.getIMax()));
+                    }
+                    else if(z.getRight() == null && z.getLeft() != null){//only left child
+                        z.setIMax(Math.max(z.getLeft().getIMax(), z.getIMax()));
+                    }else{}
+                }
+                else if(leftOrRight == 1) {  //left
+                    Node left = z.getLeft();
+                    //update right child's imax
+                    if(left.getRight() != null && left.getLeft() != null){    //both children exist
+                        int childImax = Math.max(left.getRight().getIMax(), left.getLeft().getIMax());
+                        left.setIMax(Math.max(left.getIMax(),childImax));
+                    }
+                    else if(left.getRight() != null && left.getLeft() == null) { //only right child
+                        left.setIMax(Math.max(left.getRight().getIMax(), left.getIMax()));
+                    }
+                    else if( left.getRight() == null && left.getLeft() != null){//only left child
+                        left.setIMax(Math.max(left.getLeft().getIMax(), left.getIMax()));
+                    }else{}
+
+                    // update z's imax
+                    if(z.getRight() != null && z.getLeft() != null){    //both children exist
+                        int zChildImax = Math.max(z.getRight().getIMax(), z.getLeft().getIMax());
+                        z.setIMax(Math.max(z.getIMax(),zChildImax));
+                    }
+                    else if(z.getRight() != null && z.getLeft() == null) { //only right child
+                        z.setIMax(Math.max(z.getRight().getIMax(), z.getIMax()));
+                    }
+                    else if(z.getRight() == null && z.getLeft() != null){//only left child
+                        z.setIMax(Math.max(z.getLeft().getIMax(), z.getIMax()));
+                    }
+                }
+            }
+        }
 
     }
 
@@ -59,13 +124,12 @@ public class IntervalTreap {
         //TODO
     }
 
-    /*Returns a refrence to a node in a certain interval O(log(n))*/
+    /*Returns a reference to a node in a certain interval O(log(n))*/
     public Node intervalSearch(Interval i){
         return root;    //TODO
     }
 
     /*Helper method to Right rotate*/
-    //TODO: Check for overwritting on the last if statement
     public void rightRotate(Node x){
 
         Node y = x.getParent();
